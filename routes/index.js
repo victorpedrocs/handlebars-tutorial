a26d5023b64897494939ba48ce233dc77d220de8
Victor Silva <victorpedrocorreia@gmail.com>
2017-11-10 10:28:45 -0200

    Iteration loop #each

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   let data = {
     title: 'Handlebars',
     subtitle: 'An awesome javascript template engine',
+    features: [
+      {feat: 'It is flexible'},
+      {feat: 'It\'s resourcefull '},
+    ],
   }
   res.render('index', data);
 });
