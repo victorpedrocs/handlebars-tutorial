598a84550b497e2fcc4f296d606c4c3158ed173c
Victor Silva <victorpedrocorreia@gmail.com>
2017-11-10 10:34:25 -0200

    Helpers with options

--- views/helpers/index.js
+++ views/helpers/index.js
@@ -4,6 +4,23 @@ hbs.handlebars = require('handlebars');
 const helpersObject = {
     makeLink: (text, href) => new hbs.handlebars.SafeString(`<a href="${href}">${text}</a>`),
     dangerText: text => new hbs.handlebars.SafeString(`<span class="text-danger">${text}</span>`),
+    successText: text => new hbs.handlebars.SafeString(`<span class="text-success">${text}</span>`),
+    transformText (text, options) {
+        let result = text;
+        let transf = options.hash.transformation;
+    
+        if (transf === 'upc') {
+            result = `<span class="up-case">${text}</span>`;
+        }
+        else if (transf === 'loc') {
+            result = `<span class="low-case">${text}</span>`;
+        }
+        else if (transf === 'cap') {
+            result = `<span class="capitalize">${text}</span>`;
+        }
+    
+        return new hbs.handlebars.SafeString(result);
+    },
 }
 
 
