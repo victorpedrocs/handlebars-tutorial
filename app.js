0f28afc477425d02959521fe8a379e0ab016011f
Victor Silva <victorpedrocorreia@gmail.com>
2017-11-10 10:32:37 -0200

    Helpers

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+require('./views/helpers'); // set the helpers
 
 
 // uncomment after placing your favicon in /public
--- views/helpers/index.js
+++ views/helpers/index.js
@@ -2,6 +2,7 @@ const hbs = require('hbs');
 hbs.handlebars = require('handlebars');
 
 const helpersObject = {
+    makeLink: (text, href) => new hbs.handlebars.SafeString(`<a href="${href}">${text}</a>`),
 }
 
 
